73d360197888efa6c07ad6ada4899fbb9b01133f
Vignesh S <vignura@gmail.com>
2020-12-27 13:14:54 +0530

    converted string serial processing to packet serial processing

--- SW/SrcCode/CZ-03-HomeAutomation-Slave/CZ-03-HomeAutomation-Slave.ino
+++ SW/SrcCode/CZ-03-HomeAutomation-Slave/CZ-03-HomeAutomation-Slave.ino
@@ -11,11 +11,10 @@
 #include <SoftwareSerial.h>
 #include <Relay.h>
 #include <EEPROM.h>
-/*************************************** Pin Mappings ******************************************/
-// HC-05 
-#define HC05_ST         PIN_A1    /* status pin */
-#define HC05_EN         PIN_A5    /* enable pin */
+#include "utility.h"
+#include "serial_com.h"
 
+/*************************************** Pin Mappings ******************************************/
 // Relay 
 #define RELAY_01        7       /*DRV 16 - PD7 */
 #define RELAY_02        8       /*DRV 17 - PB0 */
@@ -24,9 +23,9 @@
 #define RELAY_05        PIN_A2  /*DRV 20 - PC2 / ADC2 */
 #define MAX_RELAY_COUNT 5
 
-/* bluetooth pins */
-#define BTX_PIN         2
-#define BRX_PIN         3
+/* Master pins */
+#define MTX_PIN         2
+#define MRX_PIN         3
 
 /***********************************************************************************************/
 /* comment the below macro to disable debug prints */
@@ -68,7 +67,7 @@
 
 /****************************************** globals ********************************************/
 /* SoftwareSerial (RX, TX) */
-SoftwareSerial SS_Bluetooth(BRX_PIN, BTX_PIN);
+SoftwareSerial SS_Master(MRX_PIN, MTX_PIN);
 
 /* Relay objects pointers */
 Relay *Rly[MAX_RELAY_COUNT] = {0};
@@ -82,6 +81,7 @@ unsigned char g_ucRlyPin[MAX_RELAY_COUNT] = {RELAY_01, RELAY_02, RELAY_03, RELAY
   char g_arrcBTMsg[MAX_CMD_STRING_SIZE] = {0};
 #endif
 
+uint8_t g_address[2];
 unsigned long g_ulOnTimeSec = 0;
 unsigned char g_ucRlySel = 0;
 int g_iPwd = 0;
@@ -111,7 +111,7 @@ void setup() {
     Serial.begin(DEBUG_BUAD_RATE);
   #endif
   
-  SS_Bluetooth.begin(HC05_BUAD_RATE);
+  SS_Master.begin(HC05_BUAD_RATE);
 
   // perform self test
   SelfTest(SELF_TEST_COUNT);
@@ -192,29 +192,27 @@ void Relay_init()
 /***********************************************************************************************/
 void loop() {
 
-  char arrcCmd[MAX_CMD_STRING_SIZE] = {0};
-  int iReadBytes = 0;
-  int iCmdID = 0;
-  int iEmgStopState = 0;
+  serial_packet cmd_packet = {0};
+  serial_packet res_packet = {0};
 
-  // read data from HC-05 if available
-  iReadBytes = RecvCmd(arrcCmd, MAX_CMD_STRING_SIZE); 
+  int iReadBytes = 0;
+
+  // read data from serial port if available
+  iReadBytes = recv_packet(SS_Master, &cmd_packet); 
   if(iReadBytes > 0)
   {
     #ifdef PRINT_DEBUG
-      sprintf(g_arrcMsg, "Received: [%d] %s", iReadBytes, arrcCmd);
+      sprintf(g_arrcMsg, "Received: [%d] bytes", iReadBytes);
       Serial.println(g_arrcMsg);
     #endif
 
     // validate the command
-    if(isValidCmd(arrcCmd, iReadBytes, &iCmdID) == true)
+    if(is_valid_packet(&cmd_packet, g_address) == true)
     {
       // if valid command is received, process it
-      CmdProcess(iCmdID, g_arrcBTMsg);
+      CmdProcess(&cmd_packet, &res_packet);
 
-      #ifdef SEND_BTRES
-        SS_Bluetooth.println(g_arrcBTMsg);
-      #endif
+      send_packet(SS_Master, &res_packet);
     }
     else
     {
@@ -228,36 +226,6 @@ void loop() {
     Rly[i]->TimerTask();
   }
 
-}
-
-/***********************************************************************************************/
-/*! 
-* \fn         :: BuletoothTerminal()
-* \author     :: Vignesh S
-* \date       :: 03-DEC-2018
-* \brief      :: This function perform bluethooth terminal emulation
-* \param[in]  :: None
-* \param[out] :: None
-* \return     :: None
-*/
-/***********************************************************************************************/
-void BuletoothTerminal()
-{
-  // if data is available in bluetooth, send it to debug
-  if(SS_Bluetooth.available())
-  {
-    Serial.write(SS_Bluetooth.read());
-    // echo
-    //SS_Bluetooth.write(SS_Bluetooth.read());
-    //digitalWrite(LED_BUILTIN, HIGH);
-  }
-
-  // if data is available in debug, send it to bluetooth 
-  if(Serial.available())
-  {
-    SS_Bluetooth.write(Serial.read());
-    //digitalWrite(LED_BUILTIN, LOW);
-  }
 }
 
 /***********************************************************************************************/
@@ -303,241 +271,6 @@ void SelfTest(int iTestCount)
 
 /***********************************************************************************************/
 /*! 
-* \fn         :: PrintBytes()
-* \author     :: Vignesh S
-* \date       :: 02-DEC-2018
-* \brief      :: This function prints the buffer byte by byte 
-* \param[in]  :: ucBuffer 
-* \param[in]  :: iBuflen
-* \return     :: None
-*/
-/***********************************************************************************************/
-void PrintBytes(unsigned char *ucBuffer, int iBuflen)
-{
-  int iIndex = 0;
-
-  if(ucBuffer == NULL)
-  {
-    return;
-  }
-
-  for(iIndex = 0; iIndex < iBuflen; iIndex++)
-  {
-    #ifdef PRINT_DEBUG
-      sprintf(g_arrcMsg,"BY%0d: %#X[%c]", ucBuffer[iIndex]);
-      Serial.println(g_arrcMsg);
-    #endif
-  }
-}
-
-/***********************************************************************************************/
-/*! 
-* \fn         :: isValidCmd()
-* \author     :: Vignesh S
-* \date       :: 05-DEC-2018
-* \brief      :: This function validates the received command and return true if it a valid 
-*                command else returns false, if a valid command is received, then the 
-*                corresponding command ID is returned through out_iCmdID parameter, similarly
-*                Invalid command ID error code is returned.
-* \param[in]  :: parrcCmd, iCmdLen 
-* \param[out] :: out_iCmdID
-* \return     :: true or false
-*/
-/***********************************************************************************************/
-bool isValidCmd(char *parrcCmd, int iCmdLen, int *out_iCmdID)
-{
-  unsigned long ulHour = 0;
-  unsigned long ulMin = 0;
-  unsigned long ulSec = 0;
-  unsigned char ucRlySel = 0;
-  int iRetVal = 0;
-  int iPwd = 0;
-
-  if((parrcCmd == NULL) || (out_iCmdID == NULL) || (iCmdLen <= 0))
-  {
-    return false;
-  }
-
-  
-  if (StrnCmp(parrcCmd, CMD_STR_RELAY_ON, strlen(CMD_STR_RELAY_ON)) == true)
-  {
-    iRetVal = sscanf(parrcCmd, CMD_STR_RELAY_ON "%d", &ucRlySel);
-    if(iRetVal != 0x01)
-    {
-      // invalid command
-      #ifdef PRINT_DEBUG
-        sprintf(g_arrcMsg,"Invalid Pramater: %s", parrcCmd);
-        Serial.println(g_arrcMsg);
-      #endif
-      *out_iCmdID = CMD_INVALID_CMD_ID;
-      return false;
-    }
-    else
-    {
-      #ifdef PRINT_DEBUG
-        sprintf(g_arrcMsg,"Cmd: %s\r\nRly Sel: %d\r\n", parrcCmd, ucRlySel);
-        Serial.println(g_arrcMsg);
-      #endif
-
-      if((ucRlySel < 1) || (ucRlySel > MAX_RELAY_COUNT))
-      {
-        *out_iCmdID = CMD_INVALID_CMD_ID;
-        return false;
-      }
-
-      g_ucRlySel = ucRlySel;
-    }
-    
-    *out_iCmdID = CMD_STR_RELAY_ON_ID;
-    return true;
-  }
-  else if (StrnCmp(parrcCmd, CMD_STR_RELAY_ON_TIMER, strlen(CMD_STR_RELAY_ON_TIMER)) == true)
-  {
-    iRetVal = sscanf(parrcCmd, CMD_STR_RELAY_ON_TIMER "%d %d:%d:%d", &ucRlySel, &ulHour, &ulMin, &ulSec);
-    if(iRetVal != 0x04)
-    {
-        // invalid command
-      #ifdef PRINT_DEBUG
-        sprintf(g_arrcMsg,"Invalid Pramater: %s", parrcCmd);
-        Serial.println(g_arrcMsg);
-      #endif
-      *out_iCmdID = CMD_INVALID_CMD_ID;
-      
-      return false;
-    }
-    else
-    {
-      #ifdef PRINT_DEBUG
-        sprintf(g_arrcMsg,"Cmd: %s\r\nRly Sel: %d\r\nHour: %ld\r\nMin: %ld\r\nSec: %ld", 
-                ucRlySel, parrcCmd, ulHour, ulMin, ulSec);
-        Serial.println(g_arrcMsg);
-      #endif
-
-      if((ucRlySel < 1) || (ucRlySel > MAX_RELAY_COUNT))
-      {
-        *out_iCmdID = CMD_INVALID_CMD_ID;
-        return false;
-      }
-
-      g_ucRlySel = ucRlySel;
-      // set the global timer variable
-      g_ulOnTimeSec = (ulHour * 3600UL) + (ulMin * 60UL) + ulSec;
-    }
-   
-   *out_iCmdID = CMD_STR_RELAY_ON_TIMER_ID; 
-    return true;
-  }
-  else if (StrnCmp(parrcCmd, CMD_STR_RELAY_OFF, strlen(CMD_STR_RELAY_OFF)) == true)
-  {
-    iRetVal = sscanf(parrcCmd, CMD_STR_RELAY_OFF "%d", &ucRlySel);
-    if(iRetVal != 0x01)
-    {
-      // invalid command
-      #ifdef PRINT_DEBUG
-        sprintf(g_arrcMsg,"Invalid Pramater: %s", parrcCmd);
-        Serial.println(g_arrcMsg);
-      #endif
-      *out_iCmdID = CMD_INVALID_CMD_ID;
-      return false;
-    }
-    else
-    {
-      #ifdef PRINT_DEBUG
-        sprintf(g_arrcMsg,"Cmd: %s\r\nRly Sel: %d", parrcCmd, ucRlySel);
-        Serial.println(g_arrcMsg);
-      #endif
-
-      if((ucRlySel < 1) || (ucRlySel > MAX_RELAY_COUNT))
-      {
-        *out_iCmdID = CMD_INVALID_CMD_ID;
-        return false;
-      }
-
-      g_ucRlySel = ucRlySel;
-    }
-    
-    *out_iCmdID = CMD_STR_RELAY_OFF_ID;
-    return true;
-  }
-  else if(StrnCmp(parrcCmd, CMD_STR_START_TEST, iCmdLen) == true)
-  {
-    *out_iCmdID = CMD_STR_START_TEST_ID;
-    return true;
-  }
-  else if(StrnCmp(parrcCmd, CMD_STR_CHANGE_PWD, iCmdLen) == true)
-  {
-    iRetVal = sscanf(parrcCmd, CMD_STR_CHANGE_PWD "%d", &iPwd);
-    if(iRetVal != 0x01)
-    {
-      // invalid command
-      #ifdef PRINT_DEBUG
-        sprintf(g_arrcMsg,"Invalid Pramater: %s", parrcCmd);
-        Serial.println(g_arrcMsg);
-      #endif
-
-      *out_iCmdID = CMD_INVALID_CMD_ID;
-      return false;
-    }
-    else
-    {
-      #ifdef PRINT_DEBUG
-        sprintf(g_arrcMsg,"Cmd: %s\r\nPwd: %d", parrcCmd, iPwd);
-        Serial.println(g_arrcMsg);
-      #endif
-
-      *out_iCmdID = CMD_STR_CHANGE_PWD_ID;
-      return true;
-    }
-  }
-  else if (StrnCmp(parrcCmd, CMD_STR_RELAY_OFF_ALL, iCmdLen) == true)
-  {
-    *out_iCmdID = CMD_STR_RELAY_OFF_ALL_ID;
-    return true;
-  }
-  else
-  {
-    // invalid command
-    #ifdef PRINT_DEBUG
-      sprintf(g_arrcMsg,"Invalid Cmd: %s", parrcCmd);
-      Serial.println(g_arrcMsg);
-    #endif
-    *out_iCmdID = CMD_INVALID_CMD_ID;
-  }
-
-  return false;
-}
-
-/***********************************************************************************************/
-/*! 
-* \fn         :: StrnCmp()
-* \author     :: Vignesh S
-* \date       :: 05-DEC-2018
-* \brief      :: This function compares two strings, returns true if they are identical else
-*                false.  
-* \param[in]  :: pString1, pString2, iLen 
-* \return     :: true or false
-*/
-/***********************************************************************************************/
-bool StrnCmp(char *pString1, char *pString2, int iLen)
-{
-  if((pString1 == NULL) || (pString2 == NULL) || (iLen <= 0))
-  {
-    return false;
-  }
-
-  for(int iIndex = 0; (iIndex < iLen); iIndex++)
-  {
-    if(pString1[iIndex] != pString2[iIndex])
-    {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-/***********************************************************************************************/
-/*! 
 * \fn         :: CmdProcess()
 * \author     :: Vignesh S
 * \date       :: 05-DEC-2018
@@ -547,83 +280,67 @@ bool StrnCmp(char *pString1, char *pString2, int iLen)
 * \return     :: None
 */
 /***********************************************************************************************/
-void CmdProcess(int iCmdID, char *pResponse)
+void CmdProcess(serial_packet *cmd_packet, serial_packet *res_packet)
 { 
-  int iRetVal = 0;
   int i = 0;
+  uint8_t data_size = 0;
+  data_packets Udata_packets = {0};
 
-  if(pResponse == NULL)
+  Udata_packets.ptr = (uint16_t*)cmd_packet->data;
+
+  switch(cmd_packet->cmdID)
   {
-    return;
-  }
-
-  switch(iCmdID)
-  {
-    case CMD_STR_RELAY_ON_ID:
-      sprintf(pResponse, "%s %d", CMD_STR_RELAY_ON, g_ucRlySel);
-
-      #ifdef PRINT_DEBUG
-        sprintf(g_arrcMsg, "Turning Relay %d ON", g_ucRlySel);
-        Serial.println(g_arrcMsg);
-      #endif
-
-      Rly[g_ucRlySel -1]->setState(RELAY_ON);
-      storeRelayStates();
-    break;
-
-    case CMD_STR_RELAY_ON_TIMER_ID:
-      
-      sprintf(pResponse, "%s %d", CMD_STR_RELAY_ON_TIMER, g_ucRlySel);
-
-      #ifdef PRINT_DEBUG
-        sprintf(g_arrcMsg, "Turning Relay %d ON for %ld seconds", g_ucRlySel, g_ulOnTimeSec);
-        Serial.println(g_arrcMsg);
-      #endif
-      
-      Rly[g_ucRlySel -1]->setTimer(g_ulOnTimeSec);
-      storeRelayStates();
-    break;
-
-    case CMD_STR_RELAY_OFF_ID:
-
-      sprintf(pResponse, "%s %d", CMD_STR_RELAY_OFF, g_ucRlySel);
-
-      #ifdef PRINT_DEBUG
-        sprintf(g_arrcMsg, "Turning Relay %d OFF", g_ucRlySel);
-        Serial.println(g_arrcMsg);
-      #endif
-
-      Rly[g_ucRlySel -1]->setState(RELAY_OFF);
-    break;
-
-    case CMD_STR_START_TEST_ID:
-
-      sprintf(pResponse, "%s", CMD_STR_START_TEST);
+    case CMD_TEST_ID:
 
       // perform self test
       SelfTest(0x01);
       storeRelayStates();
+      data_size = 0;
     break;
 
-    case CMD_STR_CHANGE_PWD_ID:
-
-      sprintf(pResponse, "%s", CMD_STR_CHANGE_PWD);
-      // change Password
-      iRetVal = HC05_ChangePwd(g_iPwd);
-      if(iRetVal != 0)
-      {
-        #ifdef PRINT_DEBUG
-          sprintf(g_arrcMsg,"ERROR: Failed to change Password..!");
-          Serial.println(g_arrcMsg);
-        #endif
-      }
-    break;
-
-    case CMD_STR_RELAY_OFF_ALL_ID:
-      sprintf(pResponse, "%s", CMD_STR_RELAY_OFF_ALL);
+    case CMD_RLY_ON_ID:
 
       #ifdef PRINT_DEBUG
-        sprintf(g_arrcMsg, "Turning all Relay OFF");
+        sprintf(g_arrcMsg, "Turning Relay %d ON", Udata_packets.Prelay_on->relay_num);
+        Serial.println(g_arrcMsg);
+      #endif
+
+      Rly[Udata_packets.Prelay_on->relay_num -1]->setState(RELAY_ON);
+      storeRelayStates();
+
+      data_size = 0;
+    break;
+
+    case CMD_RLY_ON_TIMER_ID:
+      
+      #ifdef PRINT_DEBUG
+        sprintf(g_arrcMsg, "Turning Relay %d ON for %ld seconds", 
+                Udata_packets.Prelay_on_timer->relay_num, Udata_packets.Prelay_on_timer->on_time_sec);
+        Serial.println(g_arrcMsg);
+      #endif
+      
+      Rly[Udata_packets.Prelay_on_timer->relay_num -1]->setTimer(Udata_packets.Prelay_on_timer->on_time_sec);
+      storeRelayStates();
+
+      data_size = 0;
+    break;
+
+    case CMD_RLY_OFF_ID:
+
+      #ifdef PRINT_DEBUG
+        sprintf(g_arrcMsg, "Turning Relay %d OFF", Udata_packets.Prelay_off->relay_num);
+        Serial.println(g_arrcMsg);
+      #endif
+
+      Rly[Udata_packets.Prelay_off->relay_num -1]->setState(RELAY_OFF);
+
+      data_size = 0;
+    break;
+
+    case CMD_RLY_OFF_ALL_ID:
+
+      #ifdef PRINT_DEBUG
+        sprintf(g_arrcMsg, "Turning all Relays OFF");
         Serial.println(g_arrcMsg);
       #endif
 
@@ -631,115 +348,23 @@ void CmdProcess(int iCmdID, char *pResponse)
       {
         Rly[i]->setState(RELAY_OFF);
       }
+      
       storeRelayStates();
+      data_size = 0;
     break;
 
     default:
       ;// do nothing 
+
+
+    // fill the respose packet
+    res_packet->header = SERIAL_RES_HEADER;
+    res_packet->address[0] = cmd_packet->address[0];
+    res_packet->address[1] = cmd_packet->address[1];
+    res_packet->cmdID = cmd_packet->cmdID;
+    res_packet->data_size = data_size;
+    res_packet->checksum = compute_checksum(res_packet);
   }
-}
-
-/***********************************************************************************************/
-/*! 
-* \fn         :: RecvCmd()
-* \author     :: Vignesh S
-* \date       :: 07-DEC-2018
-* \brief      :: This function cheks HC-05 serial port for received data and if data is available
-*                then reads it, the number of bytes read is returned.
-* \param[in]  :: pBuff, iBuflen
-* \return     :: iIndex
-*/
-/***********************************************************************************************/
-int RecvCmd(char *pBuff, int iBuflen)
-{
-  int iIndex = 0;
-
-  if(pBuff == NULL)
-  {
-    return -1;
-  }
-  
-  while(iIndex < iBuflen)
-  {
-    delay(HC05_SERIAL_READ_DELAY_MS);
-    
-    if(SS_Bluetooth.available())
-    {
-      pBuff[iIndex] = SS_Bluetooth.read();
-      iIndex++;
-    }
-    else
-    {
-      break;
-    }
-  }
-
-  return iIndex;
-}
-
-/***********************************************************************************************/
-/*! 
-* \fn         :: HC05_ChangePwd()
-* \author     :: Vignesh S
-* \date       :: 16-SEP-2019
-* \brief      :: This function change the bluetooth paring password of the HC-05 modlue by 
-*                switching the modlue to AT mode.
-* \param[in]  :: iPwd
-* \return     :: iRetVal
-*/
-/***********************************************************************************************/
-int HC05_ChangePwd(int iPwd)
-{
-  char arrcCmd[MAX_CMD_STRING_SIZE] = {0};
-  int iRetVal = 0;
-  int iReadBytes = 0;
-
-  // validate the input 
-  if((iPwd < 0) || (iPwd > 9999))
-  {
-    return -1;
-  }
-
-  // switch the bluetooth modlue into AT mode
-  digitalWrite(HC05_EN, HIGH);
-  delay(10);
-
-  // send change password command
-  sprintf(g_arrcBTMsg, "AT+PSWD=%04d\r\n", iPwd);
-
-  #ifdef PRINT_DEBUG
-    sprintf(g_arrcMsg, "Changing Password to: %d", iPwd);    
-    Serial.println(g_arrcMsg);
-  #endif
-
-  SS_Bluetooth.write(g_arrcBTMsg);
-
-  // verify response
-  delay(10);
-  iReadBytes = RecvCmd(arrcCmd, MAX_CMD_STRING_SIZE); 
-  if(iReadBytes > 0)
-  {
-    if(StrnCmp(arrcCmd, "OK", iReadBytes) == true)
-    {
-      #ifdef PRINT_DEBUG
-        sprintf(g_arrcMsg, "Changing Password Failed\nInvalid Response: %s", arrcCmd);    
-        Serial.println(g_arrcMsg);
-      #endif
-    }
-    else
-    {
-     #ifdef PRINT_DEBUG
-        sprintf(g_arrcMsg, "Changing Password success");    
-        Serial.println(g_arrcMsg);
-      #endif 
-    }
-  }
-
-  // switch the bluetooth modlue back to normal mode
-  digitalWrite(HC05_EN, LOW);
-  delay(10);
-
-  return iRetVal;
 }
 
 /***********************************************************************************************/
@@ -766,4 +391,34 @@ void storeRelayStates()
   }
 
   EEPROM.put(((MAX_RELAY_COUNT +1) * sizeof(uint8_t)), checksum);
+}
+
+/***********************************************************************************************/
+/*! 
+* \fn         :: BuletoothTerminal()
+* \author     :: Vignesh S
+* \date       :: 03-DEC-2018
+* \brief      :: This function perform bluethooth terminal emulation
+* \param[in]  :: None
+* \param[out] :: None
+* \return     :: None
+*/
+/***********************************************************************************************/
+void BuletoothTerminal()
+{
+  // if data is available in bluetooth, send it to debug
+  if(SS_Master.available())
+  {
+    Serial.write(SS_Master.read());
+    // echo
+    //SS_Master.write(SS_Master.read());
+    //digitalWrite(LED_BUILTIN, HIGH);
+  }
+
+  // if data is available in debug, send it to bluetooth 
+  if(Serial.available())
+  {
+    SS_Master.write(Serial.read());
+    //digitalWrite(LED_BUILTIN, LOW);
+  }
 }
